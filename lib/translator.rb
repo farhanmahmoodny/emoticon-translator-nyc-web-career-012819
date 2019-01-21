536835bb1e2c09272e74e1c66534f66fcea7a6c1
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-21 00:40:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,8 +7,8 @@ def load_library(file_path)
   emoticons["get_emoticon"] = {}
   emoticons["get_meaning"] = {}
   list.each do |key, array|
-    emoticons["get_meaning"][array[1]] << key
-    emoticons["get_emoticon"][array[0]] << array[1]
+    emoticons["get_meaning"][array[1]] = key
+    emoticons["get_emoticon"][array[0]] = array[1]
   end
   emoticons
 end
