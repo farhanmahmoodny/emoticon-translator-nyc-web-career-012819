42dcf05921dc8ddac88fafd64a50797614a866ed
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-21 00:09:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,10 @@
 require "yaml"
-emoticons = YAML.load_file('./lib/emoticons.yml')
+
 
 def load_library(file_path)
+  emoticons = 
   path = YAML.load_file(file_path)
-  path
+  path.each do |key, value|
 end
 
 def get_japanese_emoticon
